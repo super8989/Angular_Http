237f0ffab015fe15506ceb2f12b030a7631835e3
Sam <super8989@gmail.com>
2021-01-14 15:35:51 -0500

    Using the catchError Operator

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 import { Post } from './post.model';
 
 // OR add this to providers: [] in app.module
@@ -45,6 +45,9 @@ export class PostsService {
             }
           }
           return postsArray;
+        }),
+        catchError((errorRes) => {
+          return throwError(errorRes);
         })
       );
     // .subscribe((posts) => {});
